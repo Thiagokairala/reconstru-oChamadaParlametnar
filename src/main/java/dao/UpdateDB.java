e6fcc28f17e1cbccb1a0f4144da63142e67b1b14
Rafael Fazzolino <rafael_fazzolino@hotmail.com>
2015-03-25 12:11:31 -0300

    Começando implementação do método de população do Banco de Dados.

---
+++ src/main/java/dao/UpdateDB.java
@@ -0,0 +1,17 @@
+package dao;
+
+public class UpdateDB {
+
+	private String nomeDeTratamento;
+	private String nomeCivil;
+	private int idParlamentar;
+	private String telefoneParlamentar;
+	private String emailParlamentar;
+	/*Aguardando conclusão do parser.*/
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Aguardando conclusão do parser.");
+	}
+
+}
