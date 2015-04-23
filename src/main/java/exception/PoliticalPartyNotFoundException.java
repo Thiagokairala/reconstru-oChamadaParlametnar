61328be76df66eff833f74933dfac35de4c7bac9
Eduardo <brasil.eduardo1@gmail.com>
2015-04-23 13:16:05 -0300

    adding exception and started TestPoliticalParty

---
+++ src/main/java/exception/PoliticalPartyNotFoundException.java
@@ -0,0 +1,12 @@
+package exception;
+
+public class PoliticalPartyNotFoundException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PoliticalPartyNotFoundException(String msg) {
+		super(msg);
+	}
+}
---
+++ src/test/java/control/TestPoliticalPartyControl.java
@@ -0,0 +1,20 @@
+package control;
+
+import model.Statistic;
+
+import org.junit.Test;
+
+import exception.PoliticalPartyNotFoundException;
+
+public class TestPoliticalPartyControl {
+	/*
+	
+	// @Test (expected = PoliticalPartyNotFoundException.class)
+	public void testGenerateStatisticPartyNotFound()
+		throws PoliticalPartyNotFoundException {
+		
+	}
+	
+	*/
+
+}
