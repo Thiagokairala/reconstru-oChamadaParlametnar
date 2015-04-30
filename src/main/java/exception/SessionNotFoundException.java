eae96607df13ab6f5df73527ee8bd35714069042
Eduardo <brasil.eduardo1@gmail.com>
2015-04-30 18:31:35 -0300

    adding session exception

---
+++ src/main/java/exception/SessionNotFoundException.java
@@ -0,0 +1,15 @@
+package exception;
+
+public class SessionNotFoundException extends Exception{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SessionNotFoundException(String msg) {
+		super(msg);
+	}
+}
+
+
+
